42d9c8775907c3c838724a507816815b01fc8391
RasmusJannerup <rj@tat.dev>
2023-11-13 19:32:05 +0100

    WIP

--- src/second_screen/view/Second.tsx
+++ src/second_screen/view/Second.tsx
@@ -1,14 +1,41 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
+import { useLayoutEffect } from "react";
 
 export const Second = () => {
 
     gsap.registerPlugin(ScrollTrigger);
 
-    // useLayoutEffect(() => {
+    useLayoutEffect(() => {
+        const timeline = gsap.timeline();
+        const innerHeight = window.innerHeight;
+        timeline.fromTo('#about-me', {
+            opacity: 0,
+            x: 100 + "%",
+        }, {
+            opacity: 1,
+            x: 0,
+        });
+        ScrollTrigger.create({
+            trigger: '#about-me',
+            start: "20% top",
+            end: `+=${innerHeight}`,
+            animation: timeline,
+            scrub: true,
+            markers: true,
+        });
 
-    // }, [])
+    }, [])
 
-    return <div className="h-screen bg-[#007663] hidden" id="second">
+    return <div className="h-screen bg-[#007663] relative hidden" id="second">
+        <div className="flex flex-col">
+            <div className="flex flex-row w-full p-10 sm:text-10xl font-bold text-6xl text-[#feefda]" id="about-me">
+                ABOUT ME
+            </div>
+            <div className="flex flex-col">
+                aiosjdoiajsdoijaoisd
+            </div>
+        </div>
+
     </div>
 }
